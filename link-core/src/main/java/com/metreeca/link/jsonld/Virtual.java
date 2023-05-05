cd5d7fe0e68edd3f972ff812349a7bb6fd4642c1
Alessandro Bollini <alessandro.bollini@metreeca.com>
2023-05-05 12:34:40 +0200

    Core / Extend JSON-LD annotation targets

--- link-core/src/main/java/com/metreeca/link/jsonld/Virtual.java
+++ link-core/src/main/java/com/metreeca/link/jsonld/Virtual.java
@@ -20,11 +20,10 @@ package com.metreeca.link.jsonld;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target({ TYPE, FIELD })
+@Target({ TYPE, FIELD, METHOD })
 @Retention(RUNTIME)
 public @interface Virtual {
 
