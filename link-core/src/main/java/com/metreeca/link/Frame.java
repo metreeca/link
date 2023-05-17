bc7280f91d4e3990518bf55d31ccb8d4f14ab6b5
Alessandro Bollini <alessandro.bollini@metreeca.com>
2023-05-17 11:09:50 +0200

    Core / Factor Frame.merge()

--- link-core/src/main/java/com/metreeca/link/Frame.java
+++ link-core/src/main/java/com/metreeca/link/Frame.java
@@ -16,11 +16,17 @@
 
 package com.metreeca.link;
 
+import java.util.Collection;
 import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.stream.Stream;
+
+import static com.metreeca.link.Query.filter;
+import static com.metreeca.link.Query.query;
+
+import static java.util.stream.Collectors.toList;
 
 public abstract class Frame<T> {
 
@@ -108,6 +114,79 @@ public abstract class Frame<T> {
 
     public Optional<Trace> validate() {
         return Optional.empty(); // !!!
+    }
+
+    public Optional<Frame<T>> merge(final Frame<T> specs) {
+
+        if ( specs == null ) {
+            throw new NullPointerException("null specs");
+        }
+
+        final Frame<T> copy=copy();
+
+        if ( copy.value().getClass().isAssignableFrom(specs.value().getClass()) ) {
+
+            copy.entries(true).forEach(entry -> {
+
+                final String field=entry.getKey();
+
+                final Object value=entry.getValue();
+                final Object model=specs.get(field);
+
+                if ( model instanceof Query ) { // merge filters
+
+                    final Query<Object> filters=query(((Query<?>)model)
+                            .filters().entrySet().stream()
+                            .map(filter -> filter(filter.getKey(), filter.getValue()))
+                            .collect(toList())
+                    );
+
+                    if ( value instanceof Query ) {
+
+                        copy.set(field, query((Query<?>)value, filters));
+
+                    } else if ( value instanceof Collection ) {
+
+                        // !!! merge specs filters
+                        // !!! handles 0/1/multiple items
+
+                        throw new UnsupportedOperationException(";( be implemented"); // !!!
+
+                    } else if ( value != null ) {
+
+                        // !!! merge specs filters
+                        // !!! ignore? report?
+
+                        throw new UnsupportedOperationException(";( be implemented"); // !!!
+
+                    }
+
+                } else if ( model != null && !(value instanceof Query) ) { // merge specs to support virtual entities
+
+                    if (
+
+                            value instanceof Boolean && value.equals(false)
+                                    || value instanceof Number && ((Number)value).intValue() == 0
+                                    || value instanceof String && ((String)value).isBlank()
+                                    || value instanceof Collection && ((Collection<?>)value).isEmpty()
+
+                    ) {
+
+                        copy.set(field, model);
+
+                    }
+
+                }
+
+            });
+
+            return Optional.of(copy);
+
+        } else {
+
+            return Optional.empty();
+
+        }
     }
 
 }
